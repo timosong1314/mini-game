7c6b161b54b99172919f9c230576dac65dc91d01
timosong1314 <41378698+timosong1314@users.noreply.github.com>
2026-07-03 11:47:37 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 import routes from './router'
 
 const router = createRouter({
-  history: createWebHistory('/mini-game/'),
+  history: createWebHashHistory(),
   routes,
 })
 
